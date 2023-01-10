376ffc70205fcd3bfdc63493149253b2daadfa7c
twi09 <victor.guyomard@irisa.fr>
2023-01-10 14:59:31 +0100

    add stability experiments

---
+++ stability_exp/run_exp.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 10:25:53 2023
+
+@author: nwgl2572
+"""
+import numpy as np 
+import matplotlib.pyplot as plt
+import torch.nn as nn
+
+
+from sklearn.datasets import  make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import torch
+from torch.utils.data import TensorDataset
+from wachter_optimal import compute_counterfactuals_wachter
+from wachter_optimal import compute_optimal_wachter
+from wachter_optimal import compute_noisy_prescribed_recourse,compute_noise_input_perturb_classic,compute_noise_input_perturb_optim
+#### LOAD DATASET
+X, y = make_moons(n_samples=10_000,noise=0.02,random_state=42)
+
+scaler = MinMaxScaler()
+X=scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+X_train_t = torch.from_numpy(X_train).to(torch.float32)
+y_train_t = torch.from_numpy(y_train).to(torch.long)
+X_test_t = torch.from_numpy(X_test).to(torch.float32)
+y_test_t = torch.from_numpy(y_test).to(torch.long)
+
+
+train_dataset = TensorDataset(X_train_t, y_train_t)
+test_dataset = TensorDataset(X_test_t, y_test_t)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=12, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=12, shuffle=False)
+
+
+class mlp_model(nn.Module):
+    def __init__(self,feature_size):
+        super(mlp_model, self).__init__()
+        self.linear_1 = torch.nn.Linear(feature_size,50)
+        self.linear_2 = torch.nn.Linear(50,2)
+        self.softmax = nn.Softmax(dim=1)
+        self.elu = nn.ELU()
+    def forward(self, x):
+        mid_outputs = self.elu(self.linear_1(x))
+        outputs = self.softmax(self.linear_2(mid_outputs))
+        return outputs
+    
+# Load trained mlp model 
+model = mlp_model(X_train.shape[1])
+model.load_state_dict(torch.load("mlp_toy_model_moons"))
+
+
+mu = 0
+sigma = 0.01
+repeat_experiment = 2 
+
+# Compute counterfactuals with Wachter 
+C = compute_counterfactuals_wachter(model,X_test_t)
+print("Compute wachter counterfactuals sucess")
+# Compute noise prescribed recourse for Wachter 
+wachter_noisy_recourse = compute_noisy_prescribed_recourse(X_test_t,C,model,mu,sigma,repeat_experiment)
+print("Compute noisy presribed recourse wachter sucess")
+
+# Compute noise input for Wachter 
+wachter_noisy_input,CC_perturbed = compute_noise_input_perturb_classic(X_test_t,C,model,mu,sigma,repeat_experiment) 
+print("Compute noisy input wachter sucess")
+
+# Save results 
+results_wachter = np.array([wachter_noisy_recourse,wachter_noisy_input])
+np.savetxt("results_wachter",results_wachter)
+
+# Compute optimal counterfactuals for different eps values 
+lr = 0.01
+max_iter = 500 
+percentage_number = 1.0
+
+Noisy_recourse = []
+Noisy_input = []
+for eps in [0.01,0.02] : 
+    C_optimal = compute_optimal_wachter(model,X_test_t,C,eps,lr,max_iter,percentage_number)
+    print("Compute wachter optimal counterfactuals sucess for eps={}".format(eps))
+    # Compute noise prescribed recourse for Wachter optim 
+    wachter_opt_noisy_recourse = compute_noisy_prescribed_recourse(X_test_t,C_optimal,model,mu,sigma,repeat_experiment)
+    # Compute noise input for Wachter optim
+    wachter_opt_noisy_input = compute_noise_input_perturb_optim(X_test_t,C_optimal,CC_perturbed,model,mu,sigma,repeat_experiment,eps,lr,max_iter,percentage_number)
+    Noisy_recourse.append(wachter_opt_noisy_recourse)
+    Noisy_input.append(wachter_opt_noisy_input)
+
+np.savetxt("Noisy_recourse_optim",np.array(Noisy_recourse))
+np.savetxt("Noisy_input_optim",np.array(Noisy_input))
+
+
+
---
+++ stability_exp/wachter_optimal.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 10:15:39 2023
+
+@author: nwgl2572
+"""
+
+from wachter import wachter_recourse
+import torch
+import numpy as np 
+
+
+def compute_counterfactuals_wachter(model,X_test_t) : 
+    y_pred_test = torch.argmax(model(X_test_t),axis=1)
+    C = []
+    for i in range(y_pred_test.shape[0]) : 
+        if y_pred_test[i] == 1 : 
+            y_target = [1,0]
+        else : 
+            y_target = [0,1]
+        ac = wachter_recourse(model,x=X_test_t[i].numpy().reshape(1,-1),y_target=y_target,cat_feature_indices=[],loss_type="BCE")
+        C.append(ac)
+    C = np.vstack(C)
+    return(C)
+
+
+from torch.autograd import Variable
+import torch.optim as optim
+import datetime
+# Function to round the perturbation
+def Adapt(final_perturb) :
+    perturb_round = np.zeros(final_perturb.shape)
+    for i in range(final_perturb.shape[1]) :
+        if np.abs(final_perturb[0][i]) > 1e-3 :
+            perturb_round[0][i] = final_perturb[0][i]
+    return(perturb_round.astype(np.float32))
+
+
+# Find a perturbation delta for a group G assigned to class pred_class
+def Optimize(model,G,pred_class,delta,lr,max_iter,percentage_number,lambda_param=0.01,t_max_min=0.5) :
+    y_target = torch.zeros(1, G.shape[0]) + 1
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # Tensors
+    G_target = torch.tensor(y_target).float()
+    lamb = torch.tensor(lambda_param).float()
+
+    # init perturb with wachter counterfactual 
+    Perturb = Variable(torch.clone(delta), requires_grad=True)
+    percentage_sucess = float(sum(model(G+Perturb)[:,1-pred_class] > 0.5) / G.shape[0])   
+
+    # Set optimizer 
+    optimizer = optim.Adam([Perturb], lr, amsgrad=True)
+
+
+    loss_fn_1 = torch.nn.BCELoss()
+
+    t0 = datetime.datetime.now()
+    t_max = datetime.timedelta(minutes=t_max_min)
+    while percentage_sucess < percentage_number : 
+        it=0
+        while percentage_sucess < percentage_number  and it < max_iter :
+            optimizer.zero_grad()
+            loss = loss_fn_1(model(G + Perturb)[:,1-pred_class].reshape(1,-1),G_target) + lamb* torch.dist(G+Perturb, G, 1)
+            loss.backward()
+            optimizer.step()
+            # Percentage of points that are effectively translated (different class)
+            percentage_sucess = float(sum(model(G+Perturb)[:,1-pred_class] > 0.5) / G.shape[0])
+            it += 1
+        lamb -= 0.05
+        if datetime.datetime.now() - t0 > t_max:
+            print("Timeout - No Counterfactual Explanation Found")
+            break
+        elif percentage_sucess >= percentage_number:
+            continue
+            #print("Counterfactual Explanation Found")
+    final_perturb = Perturb.cpu().clone().detach().numpy()
+    return(final_perturb)
+
+def compute_optimal_wachter(model,X_test_t,C,eps,lr,max_iter,percentage_number) : 
+    y_pred_test = torch.argmax(model(X_test_t),axis=1)
+    C_optimal = [] 
+    for i in range(len(X_test_t)): 
+        # Example to explain 
+        x0 = X_test_t[i]
+        # Predicted class for the example to explain
+        pred_class = y_pred_test[i]
+        # Counterfactual found with wachter
+        c_init = torch.from_numpy(C)[i]
+        # Perturbation outputed by wachter 
+        delta = c_init-x0
+        # Existing points at distance < eps 
+        cond_dist = (np.where(torch.linalg.norm(x0-X_test_t,axis=1) < eps)[0])
+        # Select only points of the same predicted class (pred_class)
+        cond = cond_dist[np.where(model(X_test_t[cond_dist])[:,pred_class] > 0.5)]
+        # Group 
+        G = X_test_t[cond]
+        # Compute the optimization problem 
+        perturb = Optimize(model,G,pred_class,delta,lr,max_iter,percentage_number)
+        # New optimal counterfactual 
+        c0 = (X_test_t[i].numpy() + perturb)
+        C_optimal.append(c0)
+    C_optimal = np.vstack(C_optimal)
+    return(C_optimal)
+
+def compute_noisy_prescribed_recourse(X_test_t,C,model,mu,sigma,repeat_experiment) : 
+        predicted_class_counterfactual = torch.argmax(model(torch.from_numpy(C)),axis=1)
+        R = []
+        for j in range(repeat_experiment) :
+            np.random.seed(j)
+            epsilon = np.random.normal(mu, sigma, X_test_t.shape)
+            predited_class_counterfactual_perturb = torch.argmax(model(torch.from_numpy(C+epsilon).float()),axis=1)
+            R.append(predicted_class_counterfactual-predited_class_counterfactual_perturb)
+        Noise = torch.mean(torch.vstack(R).float(),axis=0)
+        return(float(torch.mean(Noise)))
+
+
+def compute_noise_input_perturb_classic(X_test_t,C,model,mu,sigma,repeat_experiment) : 
+    R = []
+    CC_perturbed = []
+    for j in range(repeat_experiment) : 
+        np.random.seed(j)
+        epsilon = np.random.normal(mu, sigma, X_test_t.shape)
+        X_perturbed = (X_test_t + epsilon).float()
+        C_perturbed = compute_counterfactuals_wachter(model,X_perturbed)
+        Dist = np.linalg.norm(C-C_perturbed,axis=1,ord=1)
+        R.append(Dist)
+        CC_perturbed.append(C_perturbed)
+        
+    Noise = np.mean(np.vstack(R),axis=0)
+    return(float(np.mean(Noise)),CC_perturbed)
+
+
+
+
+def compute_noise_input_perturb_optim(X_test_t,C_optimal,CC_perturbed,model,mu,sigma,repeat_experiment,eps,lr,max_iter,percentage_number) :
+    R = []
+    for j in range(repeat_experiment) : 
+        np.random.seed(j)
+        epsilon = np.random.normal(mu, sigma, X_test_t.shape)
+        X_perturbed = (X_test_t + epsilon).float()
+        C_optimal_perturbed = compute_optimal_wachter(model,X_perturbed,CC_perturbed[j],eps,lr,max_iter,percentage_number)
+        Dist = np.linalg.norm(C_optimal-C_optimal_perturbed,axis=1,ord=1)
+        R.append(Dist)
+
+    Noise = np.mean(np.vstack(R),axis=0)
+    return(float(np.mean(Noise)))
+
+
+
+
+
